e5c2e9746622c6f4c236c18e1e569508df38db1e
Harry MacCode <emarh.harry.code@gmail.com>
2024-03-02 23:48:18 +0100

    values and setValues to refactor our code

--- src/app/features/events/form/EventForm.tsx
+++ src/app/features/events/form/EventForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
 
 type Props = {
@@ -6,17 +6,31 @@ type Props = {
 }
 
 const EventForm = ({ setFormOpen }: Props) => {
-  const [title, setTitle] = useState('');
+  const initialValues = {
+    title: '',
+    category: '',
+    description: '',
+    city: '',
+    venue: '',
+    date: ''
+  }
+  
+  const [values, setValues] = useState(initialValues);
 
   const handleSubmit = () => {
-    console.log(title);
+    console.log(values);
   }
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
   return (
     <Segment clearing>
       <Header content='Create Event' />
       <Form onSubmit={handleSubmit}>
         <Form.Field>
-            <input type='text' placeholder='Event title' value={title} onChange={e => setTitle(e.target.value)} />
+            <input type='text' placeholder='Event title' name='title' value={values.title} onChange={e => handleInputChange(e)} />
         </Form.Field>
         <Form.Field>
             <input type='text' placeholder='Category' />
